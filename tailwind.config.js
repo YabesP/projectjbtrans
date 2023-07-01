15e7580dbfc420a233ba84595bcce034900bad26
Yabes <yabesp44@gmail.com>
2023-07-01 23:21:07 +0700

    project jbtrans

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#38bdf8",
+        dark: "#0f172a",
+      },
+      fontFamily: {
+        poppins: ["Poppins"],
+        mont: ["Montserrat"],
+      },
+    },
+  },
+  plugins: [],
+};
